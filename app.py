786b32de17b5d665dc7d42302673b9dc0d57c268
asobey <alex.sobey@gmail.com>
2020-05-02 01:08:49 -0500

    Getting ready for web deployment

--- app.py
+++ app.py
@@ -26,22 +26,44 @@ def send():
         if request.form['inch']:
             inch = float(request.form['inch'])
             mm = inch * 25.4
-            to_mm = f'{inch * 25.4:.sig_figs} mm'
+            to_mm = f'{inch * 25.4:.4g} mm'
             return render_template('app.html', to_mm=to_mm)
         if request.form['mm']:
             mm = float(request.form['mm'])
-            to_inch = f'{mm / 25.4:.sig_figs} in'
+            to_inch = f'{mm / 25.4:.4g} in'
             return render_template('app.html', to_inch=to_inch)
         if request.form['lbf']:
             lbf = float(request.form['lbf'])
-            to_kgf = f'{lbf / 2.20462:.5} kgf'
-            to_N = f'{lbf * 4.44822:.5} N'
+            to_kgf = f'{lbf / 2.20462:.4g} kgf'
+            to_N = f'{lbf * 4.44822:.4g} N'
             return render_template('app.html', to_kgf=to_kgf, lbf_to_N=to_N)
         if request.form['kgf']:
             kgf = float(request.form['kgf'])
-            to_lbf = f'{kgf * 2.20462:.5} lbf'
-            to_N = f'{kgf * 9.80665:.5} N'
+            to_lbf = f'{kgf * 2.20462:.4g} lbf'
+            to_N = f'{kgf * 9.80665:.4g} N'
             return render_template('app.html', to_lbf=to_lbf, kgf_to_N=to_N)
+        # PRESSURE
+        if request.form['psi']:
+            psi = float(request.form['psi'])
+            to_Pa = f'{psi * 6894.76:.4g} Pa'
+            to_MPa = f'{psi * 0.00689476:.4g} MPa'
+            return render_template('app.html', to_Pa=to_Pa, to_MPa=to_MPa)
+        if request.form['Pa']:
+            Pa = float(request.form['Pa'])
+            to_psi = f'{Pa / 6894.76:.4g} psi'
+            to_ksi = f'{Pa / 6.89476:.4g} ksi'
+            return render_template('app.html', to_psi=to_psi, to_ksi=to_ksi)
+        # TEMPERATURE
+        if request.form['F']:
+            F = float(request.form['F'])
+            to_C = f'{(F-32)*5/9:.4g}C'
+            to_K = f'{(F-32)*5/9+273.15:.4g}K'
+            return render_template('app.html', to_C=to_C, to_K=to_K)
+        if request.form['C']:
+            C = float(request.form['C'])
+            to_F = f'{(C*9/5+32):.4g}F'
+            C_to_K = f'{(C+273.15):.4g}K'
+            return render_template('app.html', to_F=to_F, C_to_K=C_to_K)
         else:
             return render_template('app.html')
 
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+#!python3
+
+import app
+
+if __name__ == "__main__":
+    app.run()
